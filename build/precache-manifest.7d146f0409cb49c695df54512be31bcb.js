f5734a498ddf5b9e21b8e0af13e35046aaef67ec
Marianne Aguiar <mariannehaguiar@gmail.com>
2020-07-27 15:44:01 -0300

    initial config

---
+++ build/precache-manifest.7d146f0409cb49c695df54512be31bcb.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a538e162c7f004b1f48e3a6032991205",
+    "url": "/index.html"
+  },
+  {
+    "revision": "221651691f5dcd4bb31b",
+    "url": "/static/css/main.85f03f53.chunk.css"
+  },
+  {
+    "revision": "222ffcedb6c8f1c31465",
+    "url": "/static/js/2.30de73fe.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.30de73fe.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "221651691f5dcd4bb31b",
+    "url": "/static/js/main.d9b82c76.chunk.js"
+  },
+  {
+    "revision": "f6f5af218a1df8a2f2ba",
+    "url": "/static/js/runtime-main.fbf9b8bd.js"
+  },
+  {
+    "revision": "590af817c629f33c6bd79f44090684b8",
+    "url": "/static/media/logo.590af817.png"
+  }
+]);
