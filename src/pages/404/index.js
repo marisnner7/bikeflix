979b3689b41b0c5b4a37ba99faaf258e3191b7b8
Marianne Aguiar <mariannehaguiar@gmail.com>
2020-07-28 18:43:48 -0300

    function for 404 page

---
+++ src/pages/404/index.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import PageDefault from '../../components/PageDefault'
+
+function pagina404(){
+  return(
+    <PageDefault>
+
+      <div>Pagina não encontrada e achada</div>
+    </PageDefault>
+  )
+}
+
+export default pagina404;
