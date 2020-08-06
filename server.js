f8be3256546495c91975ced22107242c48822229
Marianne Aguiar <mariannehaguiar@gmail.com>
2020-08-06 17:38:10 -0300

    setting server to heroku

--- server.js
+++ server.js
@@ -1,16 +1,12 @@
-const express = require('express');
-const favicon = require('express-favicon');
-const path = require('path');
+const jsonServer = require('json-server')
+const server = jsonServer.create()
+const router = jsonServer.router('db.json')
+const middlewares = jsonServer.defaults()
+
 const port = process.env.PORT || 8080;
-const app = express();
-app.use(favicon(__dirname + '/build/favicon.ico'));
-// the __dirname is the current directory from where the script is running
-app.use(express.static(__dirname));
-app.use(express.static(path.join(__dirname, 'build')));
-app.get('/ping', function (req, res) {
- return res.send('pong');
-});
-app.get('/*', function (req, res) {
-  res.sendFile(path.join(__dirname, 'build', 'index.html'));
-});
-app.listen(port);
+
+server.use(middlewares)
+server.use(router)
+server.listen(port, () => {
+  console.log(`JSON Server is running in ${port}`)
+})
