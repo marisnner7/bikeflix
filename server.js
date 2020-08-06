1f4310d9d35dd510bdebd817616d8e991437b269
Marianne Aguiar <mariannehaguiar@gmail.com>
2020-08-06 18:06:32 -0300

    hot fix in server node.js

--- server.js
+++ server.js
@@ -3,10 +3,10 @@ const server = jsonServer.create()
 const router = jsonServer.router('db.json')
 const middlewares = jsonServer.defaults()
 
-const port = process.env.PORT || 8080;
+const port = process.env.PORT || 8081;
 
 server.use(middlewares)
 server.use(router)
 server.listen(port, () => {
   console.log(`JSON Server is running in ${port}`)
-})
+}) 
