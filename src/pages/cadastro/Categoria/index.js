70b6e1c1dd8ef97ef66380ee17eef3269f23bfbe
Marianne Aguiar <mariannehaguiar@gmail.com>
2020-08-06 22:56:14 -0300

    fixing server

--- src/pages/cadastro/Categoria/index.js
+++ src/pages/cadastro/Categoria/index.js
@@ -20,7 +20,7 @@ function CadastroCategoria() {
 
   useEffect(() => {
     const URL_TOP = window.location.hostname.includes('localhost')
-      ? 'http://localhost:8081/categorias'
+      ? 'http://localhost:8080/categorias'
       : 'https://bikeflix.herokuapp.com/categorias';
     fetch(URL_TOP)
       .then(async (resp) => {
